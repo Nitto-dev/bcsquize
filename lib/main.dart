f7b395d704599d5e672e6853c1e62d86a3e9bb6b
asmmr <asmmrocean@gmail.com>
2021-08-13 23:20:50 +0600

    chenged

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Open Trivia',
+      title: 'NittoBCS',
       theme: ThemeData(
           primarySwatch: Colors.pink,
           accentColor: Colors.indigo,
